c04d816012711522b25c600a9a9e16c6cda2f620
Rajesh <rajesh.spk@gmail.com>
2026-02-14 15:43:40 -0800

    Default portfolio

--- src/main/java/com/rbrcloud/portfoliosrvc/entity/Portfolio.java
+++ src/main/java/com/rbrcloud/portfoliosrvc/entity/Portfolio.java
@@ -23,4 +23,7 @@ public class Portfolio {
 
     @Column(nullable = false, length = 50)
     private String name;
+
+    @Column(name = "default", nullable = false)
+    private Boolean isDefault;
 }
